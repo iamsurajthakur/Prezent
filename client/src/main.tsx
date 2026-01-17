40e1cea101a244840f2b2ae08c4b597c06b4ac31
Suraj Thakur <suraj.tech.in@gmail.com>
2026-01-17 15:41:17 +0000

    feat: add Zod for environment validation and update styles

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from '@/App/App';
 import React from 'react';
-import '@/styles/index.css'
+import '@/styles/global.css'
 
 const rootElement = document.getElementById('root');
 
