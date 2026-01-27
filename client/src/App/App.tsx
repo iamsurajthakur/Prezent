a7c14427fb5e9a492eb97bf69a55dc4eee907611
Suraj Thakur <suraj.tech.in@gmail.com>
2026-01-27 09:38:26 +0000

    Add basic user flow routes

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,15 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Register from '@/pages/Register'
 import Dashboard from '@/pages/Dashboard'
 import Home from '@/pages/Home'
+import Login from '@/pages/Login'
+import { useAuthStore } from '@/States/auth.states'
 
 const App = () => {
+
+  const isAuthenticated = useAuthStore(state => state.isAuthenticated)
+
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/register' element={<Register/>} />
-      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/login' element={<Login />} />
+
+      // Protected Route
+      <Route path='/dashboard' element={ isAuthenticated ? <Dashboard /> : <Navigate to='/login' />} />
     </Routes>
     </BrowserRouter>
   )
