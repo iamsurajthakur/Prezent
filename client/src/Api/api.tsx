e689dc899af1d4fc40a7f75e5bc6350a068a8725
Suraj Thakur <suraj.tech.in@gmail.com>
2026-01-27 08:58:46 +0000

    Add frontend initial layout

---
+++ client/src/Api/api.tsx
@@ -0,0 +1,7 @@
+import axios from 'axios'
+import env from '@/Schemas/env.schema'
+
+export const api = axios.create({
+    baseURL: env.VITE_BASE_URL || 'http://localhost:5173',
+    headers: { 'Content-Type': 'application/json' },
+})
--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,9 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Register from '@/pages/Register'
+import Dashboard from '@/pages/Dashboard'
+import Home from '@/pages/Home'
+
 const App = () => {
   return (
-    <div>
-      <h1>Prezent</h1>
-    </div>
-  );
-};
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/register' element={<Register/>} />
+      <Route path='/dashboard' element={<Dashboard />} />
+    </Routes>
+    </BrowserRouter>
+  )
+}
 
-export default App;
+export default App
