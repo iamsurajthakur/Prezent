4d9580c96553a6c13926648a0000e4475128be42
Suraj Thakur <suraj.tech.in@gmail.com>
2026-02-05 16:39:57 +0000

    Add login page form

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,25 +1,56 @@
-// Login.tsx
-import AnimatedPageWrapper from '@/components/GSAP/authAnimation';
-import { Link } from 'react-router-dom';
+
 
 const Login = () => {
   return (
-    <AnimatedPageWrapper direction="left">
-      <h2 className="stagger text-3xl font-bold text-center">Login</h2>
-      <form className="stagger mt-6 flex flex-col gap-4 max-w-md mx-auto">
-        <input type="email" placeholder="Email" className="p-3 border rounded" />
-        <input type="password" placeholder="Password" className="p-3 border rounded" />
-        <button className="stagger bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition">
-          Login
-        </button>
-      </form>
-      <p className="stagger mt-4 text-center text-sm text-gray-400">
-        Don’t have an account?{' '}
-        <Link to="/register" className="text-blue-500 hover:underline">
-          Register
-        </Link>
-      </p>
-    </AnimatedPageWrapper>
+    <main className="flex bg-[#030712] h-screen items-center justify-center w-full px-4">
+            <form className="flex w-full flex-col max-w-96">
+        
+                <a href="https://prebuiltui.com" className="mb-8" title="Go to PrebuiltUI">
+                    <svg className="size-10" width="30" height="33" viewBox="0 0 30 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="m8 4.55 6.75 3.884 6.75-3.885M8 27.83v-7.755L1.25 16.19m27 0-6.75 3.885v7.754M1.655 8.658l13.095 7.546 13.095-7.546M14.75 31.25V16.189m13.5 5.976V10.212a2.98 2.98 0 0 0-1.5-2.585L16.25 1.65a3.01 3.01 0 0 0-3 0L2.75 7.627a3 3 0 0 0-1.5 2.585v11.953a2.98 2.98 0 0 0 1.5 2.585l10.5 5.977a3.01 3.01 0 0 0 3 0l10.5-5.977a3 3 0 0 0 1.5-2.585"
+                            stroke="#ffffff" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                </a>
+        
+                <h2 className="text-4xl font-medium text-gray-900">Sign in</h2>
+        
+                <p className="mt-4 text-base text-gray-500/90">
+                    Please enter email and password to access.
+                </p>
+        
+                <div className="mt-10">
+                    <label className="font-medium">Email</label>
+                    <input
+                        placeholder="Please enter your email"
+                        className="mt-2 rounded-md ring ring-gray-200 focus:ring-2 focus:ring-indigo-600 outline-none px-3 py-3 w-full"
+                        required
+                        type="email"
+                        name="email"
+                    />
+                </div>
+        
+                <div className="mt-6">
+                    <label className="font-medium">Password</label>
+                    <input
+                        placeholder="Please enter your password"
+                        className="mt-2 rounded-md ring ring-gray-200 focus:ring-2 focus:ring-indigo-600 outline-none px-3 py-3 w-full"
+                        required
+                        type="password"
+                        name="password"
+                    />
+                </div>
+        
+                <button
+                    type="submit"
+                    className="mt-8 py-3 w-full cursor-pointer rounded-md bg-indigo-600 text-white transition hover:bg-indigo-700"
+                >
+                    Login
+                </button>
+                <p className='text-center py-8'>
+                    Don't have an account? <a href="/signup" className="text-indigo-600 hover:underline">Sign up</a>
+                </p>
+            </form>
+        </main>
   );
 };
 
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,26 +1,8 @@
-// Register.tsx
-import AnimatedPageWrapper from '@/components/GSAP/authAnimation';
-import { Link } from 'react-router-dom';
+
 
 const Register = () => {
   return (
-    <AnimatedPageWrapper direction="right">
-      <h2 className="stagger text-3xl font-bold text-center">Register</h2>
-      <form className="stagger mt-6 flex flex-col gap-4 max-w-md mx-auto">
-        <input type="text" placeholder="Name" className="p-3 border rounded" />
-        <input type="email" placeholder="Email" className="p-3 border rounded" />
-        <input type="password" placeholder="Password" className="p-3 border rounded" />
-        <button className="stagger bg-green-600 text-white py-2 rounded hover:bg-green-700 transition">
-          Register
-        </button>
-      </form>
-      <p className="stagger mt-4 text-center text-sm text-gray-400">
-        Already have an account?{' '}
-        <Link to="/login" className="text-green-500 hover:underline">
-          Login
-        </Link>
-      </p>
-    </AnimatedPageWrapper>
+     <div>Register Page</div>
   );
 };
 
