ac8c14f318bae1a49bed3b5c2c75c2cc4314618d
Suraj Thakur <suraj.tech.in@gmail.com>
2026-02-06 17:19:07 +0000

    Add register form

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,4 +1,5 @@
 import Grainient from "@/components/Home/Grainient";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
@@ -29,7 +30,108 @@ const Register = () => {
           zoom={0.9}
         />
       </div>
-      
+<form className="flex w-full p-5 bg-[#0d0f0e] rounded-sm flex-col max-w-120">
+
+          <a
+          href=""
+          className="mb-8"
+          title="Go to PrebuiltUI"
+        >
+          <svg
+            className="size-10"
+            width="30"
+            height="33"
+            viewBox="0 0 30 33"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="m8 4.55 6.75 3.884 6.75-3.885M8 27.83v-7.755L1.25 16.19m27 0-6.75 3.885v7.754M1.655 8.658l13.095 7.546 13.095-7.546M14.75 31.25V16.189m13.5 5.976V10.212a2.98 2.98 0 0 0-1.5-2.585L16.25 1.65a3.01 3.01 0 0 0-3 0L2.75 7.627a3 3 0 0 0-1.5 2.585v11.953a2.98 2.98 0 0 0 1.5 2.585l10.5 5.977a3.01 3.01 0 0 0 3 0l10.5-5.977a3 3 0 0 0 1.5-2.585"
+              stroke="#ffffff"
+              strokeWidth="2.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </a>
+
+  <h2 className="text-4xl font-medium text-white">Create Account</h2>
+
+  <p className="mt-4 text-base text-gray-500/90">
+    Please fill the details below to register.
+  </p>
+
+  {/* Full Name + Email */}
+  <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-6">
+
+    <div>
+      <label className="font-medium text-white">Full Name</label>
+      <input
+        placeholder="Enter your full name"
+        className="mt-2 rounded-md ring text-white bg-transparent ring-gray-200 focus:ring-2 focus:ring-indigo-600 outline-none px-3 py-3 w-full"
+        required
+        type="text"
+        name="name"
+      />
+    </div>
+
+    <div>
+      <label className="font-medium text-white">Email</label>
+      <input
+        placeholder="Enter your email"
+        className="mt-2 rounded-md ring text-white bg-transparent ring-gray-200 focus:ring-2 focus:ring-indigo-600 outline-none px-3 py-3 w-full"
+        required
+        type="email"
+        name="email"
+      />
+    </div>
+
+  </div>
+
+  {/* Password + Confirm Password */}
+  <div className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-6">
+
+    <div>
+      <label className="font-medium text-white">Password</label>
+      <input
+        placeholder="Create password"
+        className="mt-2 rounded-md ring text-white bg-transparent ring-gray-200 focus:ring-2 focus:ring-indigo-600 outline-none px-3 py-3 w-full"
+        required
+        type="password"
+        name="password"
+      />
+    </div>
+
+    <div>
+      <label className="font-medium text-white">Confirm Password</label>
+      <input
+        placeholder="Confirm your password"
+        className="mt-2 rounded-md ring text-white bg-transparent ring-gray-200 focus:ring-2 focus:ring-indigo-600 outline-none px-3 py-3 w-full"
+        required
+        type="password"
+        name="confirmPassword"
+      />
+    </div>
+
+  </div>
+
+  <button
+    type="submit"
+    className="mt-8 py-3 w-full cursor-pointer rounded-md bg-indigo-600 text-white transition hover:bg-indigo-700"
+  >
+    Register
+  </button>
+
+  <p className="text-center text-white py-8">
+    Already have an account?{" "}
+    <Link to="/login" className="text-indigo-600 hover:underline">
+      Login
+    </Link>
+  </p>
+
+</form>
+
+
     </main>
   )
 };
