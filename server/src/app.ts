2ca32ef5d3668704a96e61b2d86c8bd93a4b76f8
Suraj Thakur <suraj.tech.in@gmail.com>
2026-02-15 11:42:59 +0000

    Add client side connection logic and user registration system

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,36 @@
 import express, {type Request,type Response} from 'express'
+import env from './config/env'
+import cors from 'cors'
 
 
 const app = express()
 
+const allowedOrigins = [
+    env.CORS_ORIGIN_DEV,
+]
+
+app.use(
+    cors({
+        origin: (origin, cb) => {
+            if (!origin) return cb(null, true)
+
+            if (allowedOrigins.includes(origin)){
+                cb(null, true)
+            }else{
+                cb(null, false)
+            }
+        },
+        credentials: true,
+    })
+)
+
+
+app.use(express.json({ limit: '20kb' }))
+app.use(express.urlencoded({ extended: true, limit: '16kb' }))
 
 app.get('/', (req: Request, res: Response) => {
     res.send('API is running...')
 })
-
-app.use(express.json({ limit: '20kb' }))
-app.use(express.urlencoded({ extended: true, limit: '16kb' }))
 
 // Import routes
 import userRouter from '@routes/auth.routes'
--- server/src/schemas/env.schemas.ts
+++ server/src/schemas/env.schemas.ts
@@ -3,4 +3,5 @@ import z from "zod";
 export const envSchema = z.object({
     MONGODB_URI: z.url(),
     PORT: z.coerce.number().default(5000),
+    CORS_ORIGIN_DEV: z.url()
 })
